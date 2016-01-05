a8a4bc2cb02b3f915d35f0d7cb7ace5b46b2f489
LT <lt@LTs-MacBook-Pro.local>
2016-01-05 11:14:20 +0800

    给少鹏君看的

---
+++ h5_second/js/page.js
@@ -0,0 +1,19 @@
+var swiper1 = new Swiper('.swiper-container-horizontal', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+    });
+
+var swiper2 = new Swiper('.swiper-container-vertical1', {
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true,
+    });
+var swiper2 = new Swiper('.swiper-container-vertical2', {
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true,
+    });
